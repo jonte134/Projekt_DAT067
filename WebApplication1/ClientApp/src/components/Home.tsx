65300a08a54ba525d008a4e87cdb30beb67a1ddb
Makalunga <47451240+Makalunga@users.noreply.github.com>
2019-11-19 20:46:39 +0100

    gjorde lite snabbt,inget att hänga i granen

--- WebApplication1/ClientApp/src/components/Home.tsx
+++ WebApplication1/ClientApp/src/components/Home.tsx
@@ -2,21 +2,35 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 const Home = () => (
-  <div>
-    <h1>Hello, world!</h1>
-        <p>Testning</p>
-        <h3>Testrubrik</h3>
+
+    <div className='backgroundcolor'>
+        <div>
+    
+        
+      
     <ul>
-      <p>antligen fungera det</p>
-    </ul>
+      
+        
+        <h5>Register</h5>
+        <form>
+
+           <p><input type='text' placeholder='First name*' name='firstName' /></p>
+            <p><input type='text' placeholder='Last name*' name='lastName' /></p>
+            <p><input type='text' placeholder='Email address*' name='email' /></p>
+        </form>
+            
+            <button type="button"
+                className="btn btn-primary btn-lg"
+                onClick={() => {  }}>
+                Create account
+                </button>
+     </ul>
+
+
    
-    <ul>
-      <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-      <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-      <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-    </ul>
-    <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-  </div>
+    </div>
+        </div>
+        
 );
 
 export default connect()(Home);
